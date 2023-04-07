24780eb3a7d4336917627742924a4a36bf0a4950
Supriyo Saha <mm.saha1982@gmail.com>
2023-04-07 17:33:51 +0530

    home final

--- src/components/Exercises.js
+++ src/components/Exercises.js
@@ -19,6 +19,22 @@ const paginate= (e, value) =>{
   window.scrollTo({ top:1800, behaviour:"smooth" })
 }
 
+useEffect(() => {
+  const fetchExercisesData = async () => {
+    let exercisesData = [];
+
+    if (bodyPart === 'all') {
+      exercisesData = await fetchData('https://exercisedb.p.rapidapi.com/exercises', exerciseOptions);
+    } else {
+      exercisesData = await fetchData(`https://exercisedb.p.rapidapi.com/exercises/bodyPart/${bodyPart}`, exerciseOptions);
+    }
+
+    setExercises(exercisesData);
+  };
+
+  fetchExercisesData();
+}, [bodyPart]);
+
   return (
     <Box id="exercises"
     sx={{mt: { lg: '110px' }}}
